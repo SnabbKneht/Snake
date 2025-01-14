6abe5743f7c1566d2698b9454f65f9561a4e274d
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-14 17:13:44 +0100

    Change snake move functions to use direction enum

--- source/snake.cpp
+++ source/snake.cpp
@@ -4,53 +4,49 @@
 
 using std::out_of_range;
 
-char snake::snake_symbol = '@';
+char snake::snake_symbol = 'O';
 
 snake::snake(grid &g, int x, int y) : g(g), pos_x(x), pos_y(y)
 {
     g.set(x, y, snake_symbol);
 }
 
-void snake::move_left()
+void snake::move(direction dir)
 {
     try
     {
-        g.set(pos_x - 1, pos_y, snake_symbol);
+        switch(dir)
+        {
+            case direction::LEFT:
+                g.set(pos_x - 1, pos_y, snake_symbol);
+                break;
+            case direction::RIGHT:
+                g.set(pos_x + 1, pos_y, snake_symbol);
+                break;
+            case direction::UP:
+                g.set(pos_x, pos_y - 1, snake_symbol);
+                break;
+            case direction::DOWN:
+                g.set(pos_x, pos_y + 1, snake_symbol);
+                break;
+        }
     }
     catch(out_of_range &err) { return; }
-    g.set(pos_x, pos_y, ' ');
-    --pos_x;
-}
 
-void snake::move_right()
-{
-    try
-    {
-        g.set(pos_x + 1, pos_y, snake_symbol);
-    }
-    catch(out_of_range &err) { return; }
     g.set(pos_x, pos_y, ' ');
-    ++pos_x;
-}
-
-void snake::move_up()
-{
-    try
+    switch(dir)
     {
-        g.set(pos_x, pos_y - 1, snake_symbol);
+        case direction::LEFT:
+            --pos_x;
+            break;
+        case direction::RIGHT:
+            ++pos_x;
+            break;
+        case direction::UP:
+            --pos_y;
+            break;
+        case direction::DOWN:
+            ++pos_y;
+            break;
     }
-    catch(out_of_range &err) { return; }
-    g.set(pos_x, pos_y, ' ');
-    --pos_y;
-}
-
-void snake::move_down()
-{
-    try
-    {
-        g.set(pos_x, pos_y + 1, snake_symbol);
-    }
-    catch(out_of_range &err) { return; }
-    g.set(pos_x, pos_y, ' ');
-    ++pos_y;
 }
--- source/snake.h
+++ source/snake.h
@@ -1,6 +1,8 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
+#include "direction.h"
+
 class grid;
 
 class snake
@@ -8,10 +10,7 @@ class snake
     public:
         snake(grid &g, int x, int y);
 
-        void move_left();
-        void move_right();
-        void move_up();
-        void move_down();
+        void move(direction);
 
     private:
         grid &g;
