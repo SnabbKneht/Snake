882c45528ce214a56534073b1a291254d3faf7f8
Kuba Kopczewski <kuba.kopczewsky@gmail.com>
2025-01-15 20:15:43 +0100

    Add bareboned food_spawner class

--- source/food_spawner.cpp
+++ source/food_spawner.cpp
@@ -1,5 +1,1 @@
-//
-// Created by kubak on 15.01.2025.
-//
-
 #include "food_spawner.h"
--- source/food_spawner.h
+++ source/food_spawner.h
@@ -1,16 +1,15 @@
-//
-// Created by kubak on 15.01.2025.
-//
-
 #ifndef FOOD_SPAWNER_H
 #define FOOD_SPAWNER_H
 
+class grid;
 
+class food_spawner
+{
+    public:
+        explicit food_spawner(grid &g) : m_grid(g) {}
 
-class food_spawner {
-
+    private:
+        grid &m_grid;
 };
-
-
 
 #endif //FOOD_SPAWNER_H
